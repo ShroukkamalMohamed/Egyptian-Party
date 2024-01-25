1d87b155f8135edca6403fc8d184e4b56e21fbda
shrouk kamal <shroukkamal15@gmail.com>
2024-01-25 22:26:47 +0200

    frist commit

--- JS/site.js
+++ JS/site.js
@@ -1,48 +1,68 @@
-// ~===================>>>>Elements==============================
-let city = document.getElementById("city");
-let day = document.getElementById("day");
-let temp = document.getElementById("temp");
-let icon = document.getElementById("icon");
-let nextforecast = document.getElementById("nextforecast");
-let saerchinput = document.getElementById("saerch");
-let btnfind = document.getElementById("btnfind");
-// ?=====================>>>>>Function===========================
-async function fetchWeatherData(cityName) {
-    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=a57df523a95c42babdd190424240401&q=${cityName}&days=4`);
-    const data = await response.json();
-    city.innerHTML = data.location.name;
-    console.log(data);
-   let nextforecastCard = "";
-    for (let index = 0; index < data.forecast.forecastday.length; index++) {
-        if (index == 0) {
-            day.innerHTML = new Date(data.forecast.forecastday[index].date).toDateString();
-            temp.innerHTML = data.forecast.forecastday[index].day.maxtemp_c + "°C";
-            icon.src = data.forecast.forecastday[index].day.condition.icon;
-        }
-        else {
-            nextforecastCard += ` 
-            <div class="nextcard card  " id="nextcard">
-            <div class="header text-center p-2">${new Date(data.forecast.forecastday[index].date).toDateString()}</div>
-                <div class="body d-flex flex-column align-items-center">
-                <div class="icon">
-                    <img src="${data.forecast.forecastday[index].day.condition.icon}" id="nexticon" alt="" />
-                </div>
-                <span id="maxtemp">${data.forecast.forecastday[index].day.maxtemp_c} °C</span>
-                <span id="minitemp">${data.forecast.forecastday[index].day.mintemp_c}°C</span>
-                <span id="status">${data.forecast.forecastday[index].day.condition.text}</span>
-                </div>
-                </div>
-                `;
-        }
-    }
-    nextforecast.innerHTML = nextforecastCard;
+// ?=====================>>>Elements===============================
+let sideMenuInnerWidth = $(".menu").innerWidth();
+let txtMessage = document.getElementById("txtMessage");
+let remain = document.getElementById("remain");
+let singerTitle = document.getElementsByClassName(" opend");
+// !=========================>>>>PageLoad==========================
+setInterval(function () {
+    var countDownDate = new Date("Jan 30, 2024 15:37:25").getTime();
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    $("#daySpan").text(days + " D");
+    $("#hoursSpan").text(hours + " h");
+    $("#minSpan").text(minutes + " m");
+    $("#secSpan").text(seconds + " s");
+
+}, 100);
+// ^=========================>>>function===========================
+function colseSideMenu() {
+    $(".sideMenu").animate({ "left": - sideMenuInnerWidth });
+    $(".status").text("Open");
 }
-fetchWeatherData("london");
-// ^====================================>>>>>Events======================================
-btnfind.addEventListener("click", function () {
-    fetchWeatherData(saerchinput.value);
-    console.log(saerchinput.value);
+function openSideMenu() {
+    $(".sideMenu").animate({ "left": 0 });
+    $(".status").text("Close");
+}
+// ~=========================>>>>>Events===========================
+
+$(".icon").click(function (e) {
+    let iconOffset = $(".sideMenu").offset().left;
+    if (iconOffset === 0) {
+        colseSideMenu();
+    }
+    else {
+        openSideMenu();
+    }
 });
-saerchinput.addEventListener("input", function () {
-    fetchWeatherData(saerchinput.value);
+$(".closeIcon").click(function (e) {
+    colseSideMenu();
 })
+
+txtMessage.addEventListener("input", function () {
+    const characterLength = 100;
+    const currentLength = txtMessage.value.length;
+    remain.innerHTML = currentLength >= 100 ? "your available character finished" : characterLength - currentLength;
+});
+
+
+function toggleSingerBody(element) {
+    var singerBody = element.nextElementSibling;
+    var isHidden = singerBody.style.maxHeight === '0px';
+
+    var allSingerBodies = document.querySelectorAll('.singerBody');
+    allSingerBodies.forEach(function (body) {
+        if (body !== singerBody) {
+            body.style.maxHeight = '0';
+        }
+    });
+
+    if (isHidden) {
+        singerBody.style.maxHeight = singerBody.scrollHeight + 'px';
+    } else {
+        singerBody.style.maxHeight = '0';
+    }
+}
